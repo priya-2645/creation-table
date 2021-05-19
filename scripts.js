19489223022bd4fcebb7593e5fa3fba64328c915
priya-2645 <priyamuruganpriya12@gmail.com>
2021-05-19 17:16:30 +0530

    fe

---
+++ scripts.js
@@ -0,0 +1,44 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+const db = new Sequelize('customer', 'root', 'Sairam3@', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+const User = db.define('User', {
+  // Model attributes are defined here
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING
+    // allowNull defaults to true
+  },
+  address: {
+    type: DataTypes.STRING
+  }
+}, {
+  // Other model options go here
+});
+
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User); // true
+db.sync({
+  force: true
+})
+  .then(() => {
+    const jane = User.create(
+      {
+        firstName: "Priya",
+      })
+  })
+  .catch((err) => console.log(err))
+
+// db.authenticate()
+//   .then(() => {
+//     console.log('connection success')
+//   })
+module.exports - {
+  db
+}
